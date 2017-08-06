accb404c5149a332a0f722421444514588cf66d4
James Cox <j-cox03@hotmail.com>
2017-08-06 16:35:34 +0100

    Square: passed context in with constructor

--- square.js
+++ square.js
@@ -1,15 +1,16 @@
-function Square(x, y, width, height){
+function Square(context, x, y, width, height){
+  this.context = context;
   this.topLeft = {x: x, y: y};
   this.topRight = {x: x + width, y: y};
   this.bottomLeft = {x: x, y: y + height};
   this.bottomRight = {x: x + width, y: y + height};
 }
 
-Square.prototype.prepare = function(context){
+Square.prototype.prepare = function(){
   this.context.rect(x, y, squareWidth, squareHeight);
 }
 
-Square.prototype.draw = function(context){
+Square.prototype.draw = function(){
   this.prepare(context);
   this.context.stroke();
 }
