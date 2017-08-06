5551a963c064da2d8b617c89f9c9fb98c1f90603
James Cox <j-cox03@hotmail.com>
2017-08-06 16:11:11 +0100

    Made method to produce a line of squares

--- run.js
+++ run.js
@@ -1,5 +1,20 @@
 window.addEventListener("load", function(){
 
-  alert("linked");
+  var canvas = document.getElementById("main-canvas");
+  var context = canvas.getContext("2d");
+  console.log(context);
 
+
+  makeSquaresHorizontal(context, 10);
 });
+
+function makeSquaresHorizontal(context, count){
+
+  var width = context.canvas.width;
+  var squareWidth = width / count;
+
+  for(var j=0; j<width; j+=squareWidth){
+    context.rect(j, 0, squareWidth, squareWidth);
+  }
+  context.stroke();
+}
