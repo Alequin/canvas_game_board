85e5d2741dc31ca7ec84ab565e7296506d70db00
James Cox <j-cox03@hotmail.com>
2017-08-06 16:41:28 +0100

    Square: made new properties for position, width and height

--- square.js
+++ square.js
@@ -1,13 +1,19 @@
 function Square(context, x, y, width, height){
+
   this.context = context;
+  this.position = {x: x, y: y};
+  this.width = width;
+  this.height = height;
+
   this.topLeft = {x: x, y: y};
   this.topRight = {x: x + width, y: y};
   this.bottomLeft = {x: x, y: y + height};
   this.bottomRight = {x: x + width, y: y + height};
+
 }
 
 Square.prototype.prepare = function(){
-  this.context.rect(x, y, squareWidth, squareHeight);
+  this.context.rect(this.position.x, this.position.y, this.width, this.height);
 }
 
 Square.prototype.draw = function(){
