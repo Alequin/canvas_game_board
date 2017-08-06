3fae4c3c6170bd6f3658d313a5a9286173627c00
James Cox <j-cox03@hotmail.com>
2017-08-06 16:21:17 +0100

    Made board object and move code to make baord into draw method

---
+++ board.js
@@ -0,0 +1,19 @@
+function Board(canvas){
+  this.canvas = canvas;
+  this.context = canvas.getContext("2d");
+  this.width = canvas.width;
+  this.height = canvas.height;
+}
+
+Board.prototype.draw = function(xCount, yCount){
+
+  var squareWidth = this.width / xCount;
+  var squareHeight = this.height / yCount;
+
+  for(var y=0; y<this.height; y+=squareHeight){
+    for(var x=0; x<this.width; x+=squareWidth){
+      this.context.rect(x, y, squareWidth, squareHeight);
+    }
+  }
+  this.context.stroke();
+}
--- run.js
+++ run.js
@@ -1,37 +1,8 @@
 window.addEventListener("load", function(){
 
   var canvas = document.getElementById("main-canvas");
-  var context = canvas.getContext("2d");
-  console.log(context);
+  var board = new Board(canvas);
 
+  board.draw(10, 10);
 
-  makeSquares(context, 10, 10);
 });
-
-function makeSquaresHorizontal(context, count){
-
-  var width = context.canvas.width;
-  var squareWidth = width / count;
-
-  for(var j=0; j<width; j+=squareWidth){
-    context.rect(j, 0, squareWidth, squareWidth);
-  }
-  context.stroke();
-}
-
-function makeSquares(context, xCount, yCount){
-
-  var width = context.canvas.width;
-  var height = context.canvas.height;
-
-  var squareWidth = width / xCount;
-  var squareHeight = height / yCount;
-
-  for(var y=0; y<height; y+=squareHeight){
-    for(var x=0; x<width; x+=squareWidth){
-      context.rect(x, y, squareWidth, squareHeight);
-    }
-  }
-
-  context.stroke();
-}
