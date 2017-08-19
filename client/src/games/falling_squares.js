8aeae4ac12923f184da9003a5b7253cc1099f4af
James Cox <j-cox03@hotmail.com>
2017-08-19 12:22:56 +0100

    FallingSquares: made basic constructor

--- client/src/games/falling_squares.js
+++ client/src/games/falling_squares.js
@@ -2,8 +2,10 @@ var Board = require("./../../board/board");
 var Animation = require("./../../animate/animate");
 var randomInt = require("./../../other/random");
 
-function FallingSquares(){
-
+function FallingSquares(container){
+  var boardContainer = document.getElementById("game-board");
+  this.board = new Board(boardContainer);
+  this.board.generateSquares(10, 10, "black", "white");
 }
 
 module.exports = FallingSquares;
