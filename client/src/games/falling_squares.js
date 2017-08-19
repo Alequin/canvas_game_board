86790159fe791c3263df01a1df3c258057ddb910
James Cox <j-cox03@hotmail.com>
2017-08-19 12:17:33 +0100

    Made file falling_squares

---
+++ client/src/games/falling_squares.js
@@ -0,0 +1,9 @@
+var Board = require("./../../board/board");
+var Animation = require("./../../animate/animate");
+var randomInt = require("./../../other/random");
+
+function FallingSquares(){
+
+}
+
+module.exports = FallingSquares;
