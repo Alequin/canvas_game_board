e44661797351446b69e202644c08e1670e2dee5b
James Cox <j-cox03@hotmail.com>
2017-08-19 02:58:45 +0100

    MovementSquare: made method to prepare first square

--- client/build/bundle.js
+++ client/build/bundle.js
@@ -610,12 +610,23 @@ function MovementSquare(){
   this.board = new Board(boardContainer);
 
   this.board.generateSquares(9, 9, "black", "white");
+  this.currentSquare = null;
 }
 
 MovementSquare.prototype.run = function(){
-  var startSquare = this.board.getSquareByPosition(4,4);
-  startSquare.style.fillColour = "red";
+  this.prepareSquare();
+}
+
+MovementSquare.prototype.prepareSquare = function(){
+  this.currentSquare = this.board.getSquareByPosition(4,4);
+  this.currentSquare.style.fillColour = "red";
   this.board.draw();
+}
+
+MovementSquare.prototype.moveSquare = function(){
+
+
+
 }
 
 module.exports = MovementSquare;
