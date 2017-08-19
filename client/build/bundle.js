811514866a014b850dd96caae8e6c32f1b6c2084
James Cox <j-cox03@hotmail.com>
2017-08-19 13:14:38 +0100

    FallingSquares: made method for removing a square when it reaches the bottom

--- client/build/bundle.js
+++ client/build/bundle.js
@@ -708,12 +708,14 @@ FallingSquares.prototype.prepareFrame = function(){
 FallingSquares.prototype.addNewSquare = function(){
   var columnIndex = randomInt(0, this.width-1);
   var newSquare = this.board.getSquareByPosition(columnIndex, 0);
+
   for(var index in this.fallingSquares){
     if(!this.fallingSquares[index]){
       this.fallingSquares[index] = newSquare;
       break;
     }
   }
+
   newSquare.style.fillColour = this.fallingSquareColour;
   newSquare.draw();
 }
@@ -723,11 +725,7 @@ FallingSquares.prototype.moveSquareDown = function(index){
   var nextSquare = this.board.getSquareBottom(1, square.position.x, square.position.y);
 
   if(!nextSquare){
-    this.fallingSquares[index] = null;
-    this.currentFalling--;
-    square.style.fillColour = "white";
-    square.removeDrawn();
-    square.draw();
+    this.removeSquare(index);
     return;
   }
 
@@ -741,6 +739,15 @@ FallingSquares.prototype.moveSquareDown = function(index){
   nextSquare.draw();
 
   this.fallingSquares[index] = nextSquare;
+}
+
+FallingSquares.prototype.removeSquare = function(index){
+  this.fallingSquares[index] = null;
+  this.currentFalling--;
+  
+  square.style.fillColour = "white";
+  square.removeDrawn();
+  square.draw();
 }
 
 module.exports = FallingSquares;
