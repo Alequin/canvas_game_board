9f536e201f7be5c1d4c75702f136d8b0b758f45a
James Cox <j-cox03@hotmail.com>
2017-09-07 22:32:40 +0100

    made test for copying the matrix of squares

--- client/src/specs/test_board.js
+++ client/src/specs/test_board.js
@@ -28,4 +28,27 @@ describe("Board", function(){
     }
   });
 
+  it("can copy matrix of squares", function(){
+    const width = 3;
+    const height = 3;
+    board.generateSquares(width,height, "", "");
+
+    const squares = board.squares;
+    const copiedSquares = board.copySquares(squares);
+
+    for(let columnIndex in squares){
+      for(let rowIndex in squares[columnIndex]){
+        const square = squares[columnIndex][rowIndex];
+        const copiedSquare = copiedSquares[columnIndex][rowIndex];
+        assert.notStrictEqual(square, copiedSquare);
+        assert.strictEqual(square.coordinates.x, copiedSquare.coordinates.x);
+        assert.strictEqual(square.coordinates.y, copiedSquare.coordinates.y);
+        assert.strictEqual(square.position.x, copiedSquare.position.x);
+        assert.strictEqual(square.position.y, copiedSquare.position.y);
+        assert.strictEqual(square.width.y, copiedSquare.width.y);
+        assert.strictEqual(square.height.y, copiedSquare.height.y);
+      }
+    }
+
+  });
 });
